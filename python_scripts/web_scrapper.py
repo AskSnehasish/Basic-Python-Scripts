1b3c9f81e5a8f50905253b522114436853daea65
Snehasish Nayak <asksnehasish@gmail.com>
2023-07-06 19:20:56 +0000

    pylint related issues resolved.

--- python_scripts/web_scrapper.py
+++ python_scripts/web_scrapper.py
@@ -1,11 +1,12 @@
-import requests
+"""Module providing Function to do get and post requests."""
+import requests as rs
 from bs4 import BeautifulSoup
 
-url_list = ["https://snehasish.dev","https://techcrunch.com"]
+url_list = ["https://snehasish.dev", "https://techcrunch.com"]
 
 for url in url_list:
     # Make a request to each url
-    response = requests.get(url)
+    response = rs.get(url,timeout=300000)
 
     # Parse the HTML content
     soup = BeautifulSoup(response.content, 'html.parser')
